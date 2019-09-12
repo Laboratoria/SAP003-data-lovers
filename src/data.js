5942e9d1e40131492006627f46c9905d62783311
KarineVerneque <karinemvs01@hotmail.com>
2019-09-12 17:53:00 -0300

    adicionando eventos e estrutura HTML

--- src/data.js
+++ src/data.js
@@ -1,22 +1,70 @@
+//manipulação de dados através de arrays e objetos.
 // esta é uma função de exemplo
 // veja como agregamos a função ao objeto global window
-
 const example = () => {
   return "example";
 };
-window.example = example;
 
 
+window.data = {
+  peru: peruData,
+  mexico: mexicoData,
+  brasil: brasilData,
+  chile: chileData
+}
+////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////
-
-//console.log(WORLDBANK.PER.indicators[0].countryCode
-
-const peruCountry = WORLDBANK.PER
-const mexicoCountry = WORLDBANK.MEX
-const brasilCountry = WORLDBANK.BRA
-const chileCountry = WORLDBANK.CHL
-
+/*
 const countries = [peruCountry, mexicoCountry, brasilCountry, chileCountry]
+*/
 
-countries.map(country => console.log(country.indicators[0].countryName));
+const peruCountry = WORLDBANK.PER.indicators
+const mexicoCountry = WORLDBANK.MEX.indicators
+const brasilCountry = WORLDBANK.BRA.indicators
+const chileCountry = WORLDBANK.CHL.indicators
+
+/*
+let filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+console.log(filtered);
+*/
+
+
+peruData = () => {
+
+  peruCountry.map(elem => console.log(elem.countryName));
+  /*
+  for (let banana in WORLDBANK){    
+    [banana].indicators.map(elem => console.log(elem.indicatorCode));
+  }
+  */
+
+  
+};
+console.log(peruData());
+
+
+
+mexicoData = () => {
+
+  mexicoCountry.map(elem => console.log(elem.indicatorCode));
+    
+};
+//console.log(mexicoData());
+
+
+
+brasilData = () => {
+
+  brasilCountry.map(elem => console.log(elem.indicatorCode));
+
+};
+//console.log(brasilData());
+
+
+
+chileData = () => {
+
+  chileCountry.map(elem => console.log(elem.indicatorCode));
+
+};
+//console.log(chileData());
--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
-window.example = example;
+//códigos que tenham a ver com a exibição dos dados na tela. Interação com o DOM. Operações como criação de nós, registro de manejadores de eventos (event listeners ou event handlers) e etc.
+//window.example = example;
+
+let buttonPeru = document.getElementById("btn-peru");
+let buttonMexico = document.getElementById("btn-mexico");
+let buttonBrasil = document.getElementById("btn-brasil");
+let buttonChile = document.getElementById("btn-chile");
+
+buttonPeru.addEventListener("click", () => {
+    let peruWrite = window.data.peru();
+    document.getElementById("peru-write").innerHTML = `Esse é o nome do país: ${peruData}`
+    
+    
+    /*
+    buttonCode.addEventListener("click", () => {
+        let encodeText = window.cipher.encode(+offSet.value, writtenMsg.value);
+        document.getElementById("result").innerHTML = `<p>Essa é sua mensagem cifrada:</p> <p>${encodeText}</p>`
+    });
+    */  
+});
+
+
+buttonMexico.addEventListener("click", () => {
+
+});
+
+
+buttonBrasil.addEventListener("click", () => {
+
+});
+
+
+buttonChile.addEventListener("click", () => {
+
+});
