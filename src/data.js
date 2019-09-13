c3f5db4211a0f0f34ce1cd6d082163f059ee998b
Karine Verneque <you@example.com>
2019-09-12 23:27:35 -0300

    Estruturando arrow function

--- src/data.js
+++ src/data.js
@@ -1,70 +1,57 @@
-//manipulação de dados através de arrays e objetos.
 // esta é uma função de exemplo
 // veja como agregamos a função ao objeto global window
 const example = () => {
   return "example";
 };
+window.example = example;
 
-
+/*
 window.data = {
   peru: peruData,
   mexico: mexicoData,
   brasil: brasilData,
   chile: chileData
-}
+}*/
 ////////////////////////////////////////////////////
-
-/*
-const countries = [peruCountry, mexicoCountry, brasilCountry, chileCountry]
-*/
 
 const peruCountry = WORLDBANK.PER.indicators
 const mexicoCountry = WORLDBANK.MEX.indicators
 const brasilCountry = WORLDBANK.BRA.indicators
 const chileCountry = WORLDBANK.CHL.indicators
 
+
+const peru = peruCountry.map
+(peruData => {
+  console.log(peruData.countryName);
+
+});
+
+document.getElementById("peru-write").innerHTML = `esse é o nome do pais ${peru.countryName}`
+
+
+const mexico = mexicoCountry.map(mexicoData => {
+  console.log(mexicoData.countryName);
+})
 /*
-let filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
-console.log(filtered);
-*/
+for (let banana in peruCountry){
+  peruCountry.map(elem => console.log(elem.indicatorName));
+}
+ }*/
+
+const brasil = brasilCountry.map(brasilData => {
+  console.log(brasilData.countryName);
+})
+
+const chile = chileCountry.map(chileData => {
+  console.log(chileData.countryName);
+})
 
 
-peruData = () => {
-
-  peruCountry.map(elem => console.log(elem.countryName));
-  /*
-  for (let banana in WORLDBANK){    
-    [banana].indicators.map(elem => console.log(elem.indicatorCode));
-  }
-  */
-
-  
-};
-console.log(peruData());
-
-
-
-mexicoData = () => {
-
-  mexicoCountry.map(elem => console.log(elem.indicatorCode));
-    
-};
-//console.log(mexicoData());
-
-
-
-brasilData = () => {
-
-  brasilCountry.map(elem => console.log(elem.indicatorCode));
-
-};
-//console.log(brasilData());
-
-
-
+/*
 chileData = () => {
 
   chileCountry.map(elem => console.log(elem.indicatorCode));
 
 };
-//console.log(chileData());
+console.log(chileData());
+*/
