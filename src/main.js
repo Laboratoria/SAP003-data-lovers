efa8f4ad34c361ec4c1b53d8ac47010e314ce522
KarineVerneque <karinemvs01@hotmail.com>
2019-09-23 18:23:10 -0300

    inicialização dos testes e atualização do código

--- src/main.js
+++ src/main.js
@@ -1,41 +1,68 @@
-//colocar as funções
-//window.data.filterTodos
+const selectPais = document.getElementById("select-country");
+selectPais.addEventListener("change", countryDatas);
 
-//btnPer.addEventListener("change", () => {
-//    let peixinho = window.data.filterTodos;
-//    document.getElementById("result").innerHTML = `Esse é o resultado ${peixinho}`;
-//    console.log(peixinho)
-//});
-//btnPer.addEventListener("click", () => {
-//    let banana = window.data.filterInfosPeru;
-//    document.getElementById("result").innerHTML = `Esse é o resultado ${banana}`
-//
-//}
-//);
+const getData = document.getElementById("select-dados");
+const getYears = document.getElementById("select-ano");
+
+const button = document.getElementById("btn-ver");
+const result = document.getElementById("result");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  const countryValue1 = selectPais.value;
+  //const dataValue1 = getData.value
+  const indicators1 = WORLDBANK[countryValue1].indicators;
+
+  const dataFiltrado = data.filterDatas(indicators1, getData.value);
+
+  dataFiltrado.map(item => {    
+    if (item.data[getYears.value] === "") {
+      result.innerHTML = "Não tem dados!";
+    } else {
+      result.innerHTML = `Esse é o resultado ${parseInt(item.data[getYears.value])}%`;
+    }
+  });
+  // filterDatas(indicators1, getData.value, getYears.value);
+});
 
 
-
-
-
-
-
-
-//códigos que tenham a ver com a exibição dos dados na tela. Interação com o DOM. Operações como criação de nós, registro de manejadores de eventos (event listeners ou event handlers) e etc.
-
-/*
-ir no HTML e pegar o que vc quer filtrar
-chamar funcao de filtro lá do data.js ---- filtro(indice, "BRA")
-pega o retorno da funcao filtro lã do data e exibe*/
-/*
-const btnBuscaPer = document.getElementById("btn-submit")
-
-btnBuscaPer.addEventListener("click", filterInfosPeru);
-
-function filterInfosPeru(e){
-    e.preventDefault();
-    const indiceSelecionado = document.getElementById("select-peru").value
-    const anoSelecionado = document.getElementById("year-peru").value
-    const objIndice = dado.PER.indicators.filter(indicador => indicador.indicatorName === indiceSelecionado)
-    objIndice.map(item => console.log(item.data[anoSelecionado]))
+function ordenaPorFavor(array) {
+  return array.sort(function (a, b) {
+    if (a.indicatorName > b.indicatorName) {
+      return 1;
+    }
+    if (a.indicatorName < b.indicatorName) {
+      return -1;
+    }
+    return 0;
+  });
 }
-*/
+
+function countryDatas(e) {
+  e.preventDefault();
+  getData.innerHTML = "";
+  getYears.innerHTML = "";
+  
+  const countryValue = selectPais.value;
+  const indicators = WORLDBANK[countryValue].indicators;
+
+//  indicators.sort(function (a, b) {
+//    if (a.indicatorName > b.indicatorName) {
+//      return 1;
+//    }
+//    if (a.indicatorName < b.indicatorName) {
+//      return -1;
+//    }
+//    return 0;
+//  });
+
+  indicators.map((elem) => {
+    getData.innerHTML += `<option>${elem.indicatorName}</option>`;
+  });
+
+  const years = WORLDBANK[countryValue].indicators[0].data;  
+  for (item of Object.keys(years)) {
+    getYears.innerHTML += `<option>${item}</option>`; 
+  }
+}
+
