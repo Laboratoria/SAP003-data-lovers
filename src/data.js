1dd57d3901520c6cc3ea4d96a839dcfae320d29e
mariacarolinafs <mariacarolina_fs@hotmail.com>
2019-09-24 09:02:42 -0300

    fiz função de filtro

--- src/data.js
+++ src/data.js
@@ -1,8 +1,27 @@
-// esta é uma função de exemplo
-// veja como agregamos a função ao objeto global window
+window.data = {
+  pais: pais,
+}
 
-const example = () => {
-  return "example";
-};
+const arrayIndicators = [
+  "SL.TLF.TOTL.FE.ZS",
+  "SL.UEM.TOTL.FE.ZS",
+  "SL.EMP.TOTL.SP.FE.ZS",
+  "SE.PRM.ENRL.FE.ZS",
+  "SE.SEC.ENRL.FE.ZS",
+  "SE.TER.CUAT.ST.FE.ZS"
+]
 
-window.example = example;
+function pais (paisEscolhido) {
+  let arrNovo = [];
+    paisEscolhido.map(i => {
+      for (let j of arrayIndicators){
+        if (j === i.indicatorCode){
+         arrNovo.push({code: j, anos: i.data, name:i.indicatorName})
+
+        }
+      }
+    })
+    return arrNovo;
+}
+
+
