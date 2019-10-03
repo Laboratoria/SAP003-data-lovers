c7d3ec33a9797fb0152a4ccd1ae825ee481aaae8
Karine Verneque <you@example.com>
2019-10-02 22:13:17 -0300

    correção do código finalizado

--- src/main.js
+++ src/main.js
@@ -1,37 +1,39 @@
-const getYears = document.getElementById("select-ano");
+const getYears = document.querySelector(".select-ano");
 
-const selectPais = document.getElementById("select-country");
+const selectPais = document.querySelector(".select-country");
 selectPais.addEventListener("change", () => {
   sortData(WORLDBANK[selectPais.value].indicators);
   countryDatas();
 });
 
-const getData = document.getElementById("select-dados");
+const getData = document.querySelector(".select-data");
 getData.addEventListener("change", () => {
   const indicators = WORLDBANK[selectPais.value].indicators;
   const dataFilter = indicators.filter(elem => elem.indicatorName === getData.value ? elem.data : null);
   inputYear(dataFilter[0].data);
 });
 
-const button = document.getElementById("btn-see");
-const yearResult = document.getElementById("year-result");
-const averageResult = document.getElementById("average-result");
+const button = document.querySelector(".btn-see");
+const yearResult = document.querySelector(".year-result");
+const averageResult = document.querySelector(".average-result");
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
+  const countryValue = selectPais.value;
+  const indicators = WORLDBANK[countryValue].indicators;
 
-  const countryValue1 = selectPais.value;
-  const indicators1 = WORLDBANK[countryValue1].indicators;
-
-  const dataFiltrado = filterDatas(indicators1, getData.value);
-  const indicatorFilter = indicators1.filter(elem => elem.indicatorName === getData.value ? elem.data : null);
-
+  const dataFiltrado = filterDatas(indicators, getData.value);
+  const indicatorFilter = indicators.filter(elem => elem.indicatorName === getData.value ? elem.data : null);
   dataFiltrado.map(item => item.data[getYears.value] === "" ? yearResult.innerHTML = "Não tem dados!" : yearResult.innerHTML = `<h4>Média anual ${parseInt(item.data[getYears.value])}%</h4>`);
 
   const years = indicatorFilter[0].data;
   const dataOfYear = dataFiltrado[0].data[getYears.value];
   averageResult.innerHTML = `<h4>Média por anos apurados ${parseInt(dataAverage(years))}%</h4>`;
 
+  createChart(dataOfYear, years);
+});
+
+function createChart(dataOfYear, years) {
   const ctx = document.getElementById("myChart").getContext("2d");
 
   new Chart(ctx, {
@@ -68,25 +70,27 @@ button.addEventListener("click", (e) => {
 
   window.scroll({
     top: 900,
-    behavior: 'smooth'
+    behavior: "smooth"
   });
-});
+};
 
 function countryDatas() {
-  getData.innerHTML = '<option value="" disabled="" selected=""> Selecione um dado indicador</option>';
+  getData.innerHTML = "<option value=\"\" disabled=\"\" selected=\"\"> Selecione um dado indicador</option>";
   const countryValue = selectPais.value;
   const indicators = WORLDBANK[countryValue].indicators;
 
-  indicators.map((elem) => {
-    getData.innerHTML += `<option>${elem.indicatorName}</option>`;
-  });
-}
+  const mapIndicatorsName = indicators.map((elem) => {
+    return `<option>${elem.indicatorName}</option>`;
+  }).join("");
+  getData.innerHTML = mapIndicatorsName;
+};
 
 function inputYear(years) {
-  getYears.innerHTML = '<option value="" disabled="" selected=""> Selecione o ano apurado</option>';
+  getYears.innerHTML = "<option value=\"\" disabled=\"\" selected=\"\"> Selecione o ano apurado</option>";
   const filterYear = Object.entries(years).filter(ano => ano[1] !== "");
 
-  filterYear.map((elem) => {
-    getYears.innerHTML += `<option>${elem[0]}</option>`;
-  });
-}
+  const mapYears = filterYear.map((elem) => {
+    return `<option>${elem[0]}</option>`;
+  }).join("");
+  getYears.innerHTML = mapYears;
+};
