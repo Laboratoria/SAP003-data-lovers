73ce09dbdc8c6b9e32118e16aecfbf1cbcbb7422
GeniferGonzalezSantos <51093689+GeniferGonzalezSantos@users.noreply.github.com>
2020-08-17 20:03:17 -0300

    Revert "Developer"

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,6 @@
-require("../src/Js/data.js");
+require("../src/data.js");
 
-const chosenIndicators = [
+const arrayIndicators = [
   "SL.TLF.TOTL.FE.ZS",
   "SL.UEM.TOTL.FE.ZS",
   "SL.EMP.TOTL.SP.FE.ZS",
@@ -9,53 +9,49 @@ const chosenIndicators = [
   "SE.TER.CUAT.ST.FE.ZS"
 ];
 
-test('function that takes database data from selected indicators', () => {
-  expect(showSelectCountry()).toBe("function");
+describe("teste da função filtro", () => {
+  it("é uma função", () => {
+    expect(typeof window.data.pais([{ chave: 1}])).toBe("object");
+  });
+});
+describe("teste da função calculo", () => {
+  it("é um função", () => {
+    expect(typeof data.calculo).toBe("function");
+  });
+  it("é um função", () => {
+    expect(data.calculo(arr, "xuxu", "banana")).toEqual(100);
+  });
 });
 
-// describe("teste da função filtro", () => {
-//   it("é uma função", () => {
-//     expect(typeof window.data.pais([{ chave: 1}])).toBe("object");
-//   });
-// });
-// describe("teste da função calculo", () => {
-//   it("é um função", () => {
-//     expect(typeof data.calculo).toBe("function");
-//   });
-//   it("é um função", () => {
-//     expect(data.calculo(arr, "xuxu", "banana")).toEqual(100);
-//   });
-// });
-
-// const arr = [
-//   {
-//     "data": {
-//       "2000": "",
-//       "2001": "",
-//       "2002": 31.4799995422363,
-//       "2003": 29.6299991607666,
-//       "2004": 27.6299991607666,
-//     },
-//     "indicatorCode": "xuxu"
-//   },
-//   {
-//     "data": {
-//       "2000": "",
-//       "2001": "",
-//       "2002": 31.4799995422363,
-//       "2003": 29.6299991607666,
-//       "2004": 27.6299991607666,
-//     },
-//     "indicatorCode": "banana"
-//   },
-//   {
-//     "data": {
-//       "2000": "",
-//       "2001": "",
-//       "2002": 31.4799995422363,
-//       "2003": 29.6299991607666,
-//       "2004": 27.6299991607666,
-//     },
-//     "indicatorCode": "peixinho"
-//   },
-// ];
+const arr = [
+  {
+    "data": {
+      "2000": "",
+      "2001": "",
+      "2002": 31.4799995422363,
+      "2003": 29.6299991607666,
+      "2004": 27.6299991607666,
+    },
+    "indicatorCode": "xuxu"
+  },
+  {
+    "data": {
+      "2000": "",
+      "2001": "",
+      "2002": 31.4799995422363,
+      "2003": 29.6299991607666,
+      "2004": 27.6299991607666,
+    },
+    "indicatorCode": "banana"
+  },
+  {
+    "data": {
+      "2000": "",
+      "2001": "",
+      "2002": 31.4799995422363,
+      "2003": 29.6299991607666,
+      "2004": 27.6299991607666,
+    },
+    "indicatorCode": "peixinho"
+  },
+];
